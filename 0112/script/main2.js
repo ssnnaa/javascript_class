5d513e3467653ae27bd7c92226b91681c6db9792
ssnnaa <kim6228s2@gmail.com>
2023-01-25 13:21:27 +0900

    jquery

--- 0112/script/main2.js
+++ 0112/script/main2.js
@@ -15,18 +15,26 @@ $(function(){
   //메인 좌,우 슬라이드
   let l_btn=$('.slide i.fa-angle-left')
   let r_btn=$('.slide i.fa-angle-right')
+  let img_w =$('.slide img').width();
+
   //1. 5번 이미지를 1번의 앞쪽으로 자리를 재배치한다.
   $('.slide li:last-child').insertBefore('.slide li:first-child');
-  $('.slide ul').css('margin-left','-1600px');
+  $('.slide ul').css('margin-left',-img_w);
+
+  //브라우저창 크기가 변하면
+  $(window).resize(function(){
+    //이미지 크기를 다시 계산하여 변수에 대입한다.
+    img_w = $('.slide img').width();
+  });
 
   //2. 움직이는 함수
   function moveLeft(){
     // console.log('왼쪽으로 이동');
     //왼쪽으로 3200만큼 이동하고
-    $('.slide ul').animate({'margin-left':'-3200px'},500,function(){
+    $('.slide ul').animate({'margin-left':-2*img_w},500,function(){
       // 왼쪽 첫번째 li태그를 마지막 li태그의 뒤쪽에 자리를 옮긴다.
       $('.slide li:first-child').insertAfter('.slide li:last-child');
-      $('.slide ul').css('margin-left','-1600px');
+      $('.slide ul').css('margin-left',-img_w);
     });
   }
 
@@ -34,7 +42,7 @@ $(function(){
     $('.slide ul').animate({'margin-left':'0px'},500,function(){
       //오른쪽 마지막 li태그를 왼쪽 첫번째 li태그의 앞에 자리를 ㅗㄹㅇㅁ긴다.
       $('.slide li:last-child').insertAfter('.slide li:first-child');
-      $('.slide ul').css('margin-left','-1600px');
+      $('.slide ul').css('margin-left',-img_w);
     });
   }
 
